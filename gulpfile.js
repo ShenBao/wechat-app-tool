3498cf2e7a537297e68b07637ee7736a4e11c573
ShenBao <shenbaoone@gmail.com>
2018-07-14 13:43:59 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 /*
- * @Author: ShenBao 
- * @Date: 2018-07-010 16:35:57 
+ * @Author: ShenBao
+ * @Date: 2018-07-010 16:35:57
  * @Last Modified by: ShenBao
- * @Last Modified time: 2018-07-11 23:34:45
+ * @Last Modified time: 2018-07-14 13:42:19
 */
 
 const gulp = require('gulp');
@@ -14,44 +14,44 @@ const imagemin = require('gulp-imagemin');
 const gulpWatch = require('gulp-watch');
 
 const config = {
-    stylePath: './src/styles',
+    stylesPath: './src/styles',
     pagesPath: ['./src/styles/pages/**/*.scss'],
     outputPagesPath: './weapp/pages',
     componentsPath: ['./src/styles/components/**/*.scss'],
     outputComponentsPath: './weapp/components',
-    stylePath: './src/styles/styles/**/*',
-    outputStylePath: './weapp/styles',
+    commonStylesPath: './src/styles/styles/**/*',
+    outputCommonStylesPath: './weapp/styles'
 };
 
 const sassStyleHandler = (entry, output) => {
-    gulp.src(entry)
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    gulp
+        .src(entry)
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(autoprefixer())
-        .pipe(cssmin({
-            advanced: true,
-            keepSpecialComments: '*'
-        }))
-        .pipe(rename({
-            extname: ".wxss"
-        }))
+        .pipe(cssmin({advanced: true, keepSpecialComments: '*'}))
+        .pipe(rename({extname: ".wxss"}))
         .pipe(gulp.dest(output));
 };
 
 gulp.task('style', () => {
     sassStyleHandler(config.pagesPath, config.outputPagesPath);
     sassStyleHandler(config.componentsPath, config.outputComponentsPath);
-    sassStyleHandler(config.stylePath, config.outputStylePath);
+    sassStyleHandler(config.commonStylesPath, config.outputCommonStylesPath);
 });
 
 gulp.task('img', () => {
-    gulp.src('./src/imgs/**/*')
+    gulp
+        .src('./src/imgs/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./weapp/imgs'));
 
 });
 
 gulp.task('watch', ['style'], () => {
-    return gulpWatch(config.stylePath, () => {
+    // gulp.watch([
+    //     config.stylePath, config.pagesPath, config.componentsPath, config.stylePath
+    // ], ['style']);
+    return gulpWatch(config.stylesPath, () => {
         gulp.start('style');
     });
 });
